2184e01b28c3660d89014d9c1e97c21ee6bf0beb
Hyeokzins <128476961+Hyeokzins@users.noreply.github.com>
2023-03-31 13:42:06 +0900

    Create chapter6_ex6.py

---
+++ chapter6/chapter6_ex6.py
@@ -0,0 +1,5 @@
+age=40;name='sangchul choi'
+print("I'm {0} years old.".format(age))
+print("my name is {0} and {1} years old.".format(name,age))
+
+print("product: {0}, price per unit: {1:.2f}.".format("apple", 5.234))
