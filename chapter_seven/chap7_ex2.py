daa21b07b407215eb9c7fcf87e0ab2571502a502
Hyeokzins <128476961+Hyeokzins@users.noreply.github.com>
2023-03-31 17:01:43 +0900

    Add files via upload

---
+++ chapter_seven/chap7_ex2.py
@@ -0,0 +1,10 @@
+countrycode = {}
+countrycode= {"America":1, 'korea':82,'china':86,'japan':85}
+
+print(countrycode)
+
+print(countrycode.keys())
+
+print(countrycode.values())
+
+print(countrycode.items())
